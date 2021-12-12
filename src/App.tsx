62144ac1bc64958e5fcfdf97b4ff66240fbddc8d
webDesignMumeishon <muma.sanmartin2011@gmail.com>
2021-12-12 14:06:17 -0700

    [add] basic structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+
+/**
+ * Challenge: build the basic structure of our game
+ * 
+ * 1. <h1> title at the top
+ * 2. <textarea> for the box to type in 
+ *      (tip: React normalizes <textarea /> to be more like <input />, 
+ *      so it can be used as a self-closing element and uses the `value` property
+ *      to set its contents)
+ * 3. <h4> ti display the amount of time remaining
+ * 4. <button> to start the game
+ * 5. Another <h1> to display the word count
+ */
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <h1>Speed Typing Game</h1>
+      <textarea name="" id="" cols="30" rows="10"/>
+      <h4>[Remaining Time]</h4>
+      <button>Start Game</button>
+      <h1>Number of Words</h1>
+    </main>
+  )
 }
 
 export default App;
