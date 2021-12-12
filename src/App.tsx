fe5020b9c3ac2f9164a2bce9fb6c639b5897e2c8
webDesignMumeishon <muma.sanmartin2011@gmail.com>
2021-12-12 14:16:45 -0700

    [add] controllet textarea

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './App.css';
 
 
-/**
- * Challenge: build the basic structure of our game
- * 
- * 1. <h1> title at the top
- * 2. <textarea> for the box to type in 
- *      (tip: React normalizes <textarea /> to be more like <input />, 
- *      so it can be used as a self-closing element and uses the `value` property
- *      to set its contents)
- * 3. <h4> ti display the amount of time remaining
- * 4. <button> to start the game
- * 5. Another <h1> to display the word count
- */
-
 function App() {
+  const [state, setState] = useState("")
+
+  const handleOnChange = (e : React.ChangeEvent<HTMLTextAreaElement>) => {
+    const {value} = e.target
+    setState(value)
+  }
+
+  console.log(state)
+
   return (
     <main>
       <h1>How fast can you type?</h1>
-      <textarea/>
+      <textarea value={state} onChange={handleOnChange}/>
       <h4>Time remaining: []</h4>
       <button>Start Game</button>
       <h1>Word Count: ??</h1>
