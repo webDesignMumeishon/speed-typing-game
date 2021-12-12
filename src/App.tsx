642aa4b01fcdee358167a4dd22d18a506b04f332
webDesignMumeishon <muma.sanmartin2011@gmail.com>
2021-12-12 14:21:25 -0700

    [change] variables names

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,18 @@ import './App.css';
 
 
 function App() {
-  const [state, setState] = useState("")
+  const [text, setState] = useState<string>("")
 
   const handleOnChange = (e : React.ChangeEvent<HTMLTextAreaElement>) => {
     const {value} = e.target
     setState(value)
   }
 
-  console.log(state)
 
   return (
     <main>
       <h1>How fast can you type?</h1>
-      <textarea value={state} onChange={handleOnChange}/>
+      <textarea value={text} onChange={handleOnChange}/>
       <h4>Time remaining: []</h4>
       <button>Start Game</button>
       <h1>Word Count: ??</h1>
