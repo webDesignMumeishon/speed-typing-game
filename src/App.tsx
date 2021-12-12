9a23f0ca82039926b7624a7bf59d4d86c29eff3b
webDesignMumeishon <muma.sanmartin2011@gmail.com>
2021-12-12 14:52:18 -0700

    [add] countdown & setTimeout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
-import React,{useState} from 'react';
+import { count } from 'console';
+import React,{useState, useEffect} from 'react';
 import './App.css';
+
+/**
+ * Challenge:
+ * 
+ * 1. Create state to hold the current value of the countdown timer.
+ *    Display this time in the "Time Remaining" header
+ * 
+ * 2. Set up an effect that runs every time the `timeRemaining` changes
+ *    The effect should wait 1 second, then decrement the `timeRemaining` by 1
+ * 
+ *    Hint: use `setTimeout` instead of `setInterval`. This will help you avoid
+ *    a lot of extra work.
+ * 
+ *    Warning: there will be a bug in this, but we'll tackle that next
+ */
+
 
 
 function App() {
   const [text, setText] = useState<string>("")
+  const [countdown, setCountdown] = useState<number>(5)
+
 
   const handleOnChange = (e : React.ChangeEvent<HTMLTextAreaElement>) => {
     const {value} = e.target
@@ -15,12 +34,23 @@ function App() {
     console.log(total)
   }
 
+  useEffect(() => {
+    if(countdown > 0){
+      setTimeout(() => {
+        setCountdown(prevCount => prevCount - 1)
+      }, 1000)
+    }
+
+ 
+
+  }, [countdown])
+
 
   return (
     <main>
       <h1>How fast can you type?</h1>
       <textarea value={text} onChange={handleOnChange}/>
-      <h4>Time remaining: []</h4>
+      <h4>Time remaining: {countdown}</h4>
       <button>Start Game</button>
       <h1 onClick={() => calculateWordCount()}>Word Count: ??</h1>
     </main>
