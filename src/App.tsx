62ac2f04e85ce134d5bed1b0e2e908651852794f
webDesignMumeishon <muma.sanmartin2011@gmail.com>
2021-12-12 15:23:32 -0700

    [add] countdown features

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,62 @@
-import { count } from 'console';
 import React,{useState, useEffect} from 'react';
 import './App.css';
+
 
 /**
  * Challenge:
  * 
- * 1. Create state to hold the current value of the countdown timer.
- *    Display this time in the "Time Remaining" header
+ * When the timer reaches 0, count the number of words the user typed in 
+ * and display it in the "Word count" section
  * 
- * 2. Set up an effect that runs every time the `timeRemaining` changes
- *    The effect should wait 1 second, then decrement the `timeRemaining` by 1
- * 
- *    Hint: use `setTimeout` instead of `setInterval`. This will help you avoid
- *    a lot of extra work.
- * 
- *    Warning: there will be a bug in this, but we'll tackle that next
+ * After the game ends, make it so the user can click the Start button again
+ * to play a second time
  */
-
-
 
 function App() {
   const [text, setText] = useState<string>("")
   const [countdown, setCountdown] = useState<number>(5)
-
+  const [isTimeRunning, setIsTimeRunning] = useState<boolean>(false)
+  const [wordCount, setWordCount] = useState<number>(0)
 
   const handleOnChange = (e : React.ChangeEvent<HTMLTextAreaElement>) => {
     const {value} = e.target
     setText(value)
   }
 
-  const calculateWordCount = () => {
-    const total = text.trim().split(' ').filter(word => word !== "").length
-    console.log(total)
+  const calculateWordCount = () : number => {
+    const total : number = text.trim().split(' ').filter(word => word !== "").length
+    return total
+  }
+
+  const startGame = () => {
+    setIsTimeRunning(true)
+    if(countdown === 0){
+      setCountdown(5)
+      setWordCount(0)
+      setText("")
+    }
   }
 
   useEffect(() => {
-    if(countdown > 0){
+    if(isTimeRunning && countdown > 0){
       setTimeout(() => {
         setCountdown(prevCount => prevCount - 1)
       }, 1000)
     }
-
- 
-
-  }, [countdown])
-
+    else if(countdown === 0){
+      setIsTimeRunning(false)
+      const totalWords = calculateWordCount()
+      setWordCount(() => totalWords)
+    }
+  }, [countdown, isTimeRunning])
 
   return (
     <main>
       <h1>How fast can you type?</h1>
       <textarea value={text} onChange={handleOnChange}/>
       <h4>Time remaining: {countdown}</h4>
-      <button>Start Game</button>
-      <h1 onClick={() => calculateWordCount()}>Word Count: ??</h1>
+      <button onClick={() => startGame()}>Start Game</button>
+      <h1 onClick={() => calculateWordCount()}>Word Count: {wordCount}</h1>
     </main>
   )
 }
